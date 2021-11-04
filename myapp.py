2e9ca83f39d387cafc3e81ba1a8e1ec968c86ec9
knock_nott <77430863+Nikhil-Jagtap619@users.noreply.github.com>
2021-11-04 13:38:45 +0530

    Add files via upload

--- myapp.py
+++ myapp.py
@@ -19,14 +19,32 @@ def openlocation():
         messagebox.showwarning("Warning!","please choose a folder!")
 
 #download video funtion
-def downloadVideo():
+# def downloadVideo():
+#     choice = qualityChoice.get()
+#     error.config(text="url found", fg="green")
+#     messagebox.showinfo("YT downloader", "you've selected " + choice)
+#     if (str(choice) == qualityChoice[0]) or (str(choice) == qualityChoice[1]):
+#         url = YouTube(str(entryBox.get()))
+#         video = url.streams.filter(progressive=True, file_extension="mp4").first()
+#         video.download(folder_name)
+
+#         messagebox.showwarning("Alert!", "Download Complete!")
+#     elif choice == qualityChoice[2]:
+#         url = YouTube(str(entryBox.get()))
+#         music = url.streams.filter(progressive=True, only_audio=True).first()
+#         music.download(folder_name)
+#     else:
+#         messagebox.showwarning("Warning", "Please select your quality!")
+
+
+def downloader():
     choice = qualityChoice.get()
     error.config(text="url found", fg="green")
     url = YouTube(str(entryBox.get()))
-    video = url.streams.filter(progressive=True, file_extension="mp4", progressivebar=True).first()
+    video = url.streams.filter(progressive=True, file_extension="mp4").first()
+    messagebox.showinfo("download quality", "You selected "+choice )
     video.download(folder_name)
-
-    messagebox.showwarning("Alert!", "Download Complete!")
+    messagebox.showwarning("status", "Download Complete!")
 
 
         
@@ -42,12 +60,12 @@ root.iconphoto(False, image_icon)
 
 #link paste label
 ytLabel = Label(root, text="Enter your URL here", font=("Helvetica", 14))
-ytLabel.grid()
+ytLabel.grid(pady=(10))
 
 #enter box for url pasting
 ytEntryVar = StringVar()
 entryBox = Entry(root, width=50, textvariable=ytEntryVar)
-entryBox.grid()
+entryBox.grid(pady=(10))
 
 #error mgs box
 #error = messagebox.showwarning("incorrect url")
@@ -60,7 +78,7 @@ saveLabel.grid()
 
 #make a button to open the directory
 saveEntry = Button(root, text="choose path", width=10, bg="#343a40", fg="#ff4d6d", command=openlocation)
-saveEntry.grid()
+saveEntry.grid(pady=(10))
 
 #location error if user doesnt choose the path
 #locationError = messagebox.showwarning("choose your destination")
@@ -74,12 +92,15 @@ qualityLabel.grid()
 #choices of video quality
 choices = ["720p", "144p", "only audio"]
 qualityChoice = ttk.Combobox(root, values=choices)
-qualityChoice.grid()
+qualityChoice.grid(pady=(20))
+
 
 #download button
-download = Button(root, text="Download", bg="#343a40", fg="#ff4d6d", width=10, command=downloadVideo)
+download = Button(root, text="Download", bg="#343a40", fg="#ff4d6d", width=10, command=downloader)
 download.grid()
 
+credit = Label(root, text="Made by Senpai_knock", fg="#f28482", font=("impact", 10))
+credit.grid(pady=(40))
 
 
 root.mainloop()
